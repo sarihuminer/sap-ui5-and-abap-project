6ebfff7f6ecc7f56706a65fe35c306d38d5d44af
sari shteren <sh6197174@gmail.com>
2020-08-26 17:06:46 +0300

    fragment

--- WebContent/controller/View2.controller.js
+++ WebContent/controller/View2.controller.js
@@ -8,6 +8,7 @@ sap.ui.define([
 		onInit: function () {
 			// Set the initial form to be the display one
 			this._showFormFragment("Form2");
+			this.create_newOrder();
 		},
 		_formFragments: {},
 		_showFormFragment : function (sFragmentName) {
@@ -35,7 +36,40 @@ sap.ui.define([
 			sUrl = sPath;
 			return sUrl;
 			},
+			create_newOrder: function() {
+				var that = this;
+				var sServiceUrl = this.getServiceUrl();
+				var oModel = new
+				sap.ui.model.odata.ODataModel(sServiceUrl,
+				true);
+				var oFilter = [];
+				
+				//oFilter.push(new sap.ui.model.Filter('IvEbeln',sap.ui.model.FilterOperator.EQ,'0000000001'));
+				var oData={};
+				var f=this._getFormFragment("Form2");
+				console.log(f);
+				
+				var Pname = this.getView().byId("productName")._lastValue;
+				var amount = this.getView().byId("amount")._lastValue;
+				var price = this.getView().byId("price")._lastValue;
+				console.log("prod name " +Pname+" amount "+amount+" price "+price);
+				
+				oData.Amount = amount;
+				oData.EBELN ="1";
+				oData.EBELP ="0005";
+				//oData.NAME = Pname;
+				oData.NAME = "קפקפי אצבע";
+				oData.PRICE="50";
+				
+				oModel.create("/EtAddEkpoSet", oData, {
+					  success: function(oCreatedEntry) {
+					  console.log("succses!");
+					  }, 
+					  error: function(oError) { 
+console.log("erorr !!!!!!  :( "); }
+					});
 			
+				}
 			
 			
 	});
